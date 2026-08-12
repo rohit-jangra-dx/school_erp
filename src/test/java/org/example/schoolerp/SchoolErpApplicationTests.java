635f9a6eb76f0b8d35739547f9d546e34867268c
rohit-jangra-dx <rjangradev@gmail.com>
2026-08-12 15:55:43 +0530

    docker compose is ready, fully containerizable app

---
+++ src/test/java/org/example/schoolerp/SchoolErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.schoolerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
